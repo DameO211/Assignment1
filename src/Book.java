fc8f97dd7913d7370a4949a566c60d6286b304f7
dameoncole <DameO211@gmail.com>
2021-03-24 07:32:04 -0400

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    //    Attribute
+    private int id;
+    private String title;
+    private int price;
+
+    public Book(int bookID,String bookTitle,int bookPrice) {
+        this.id = bookID;
+        this.title = bookTitle;
+        this.price = bookPrice;
+    }
+
+}
---
+++ src/Inventory.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Inventory {
+
+    public void addR() {
+        Scanner addRequest = new Scanner(System.in);
+        System.out.println("Please enter book data ");
+//    Add: Prompts user for book data and add to the inventory list.
+//    If the book already exists (based on id value),
+//    the add request will fail and an error message will be printed to the console
+    }
+
+    public void remove() {
+//        Prompts user for book id
+        Scanner removeRequest = new Scanner(System.in);
+        System.out.println("Please enter book id ");
+
+//    b. Remove.  finds the id in the inventory list and removes it.
+//        If the book matching the id is not in the inventory,
+//        remove request will fail and an error message will be printed to the console
+    }
+
+
+    public void Find() {
+        Scanner findRequest = new Scanner(System.in);
+        System.out.println("Please enter book id ");
+//    c. Find: Prompts user for book id, finds the id in the inventory list and print all the data for the book
+//            (id, title, and price). If the book matching the id is not in the inventory,
+//            find request will fail and an error message will be printed to the console
+    }
+
+    public void display(){
+//        Print all the book information for each book to the console
+    }
+}
